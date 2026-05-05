f27ed7896d1043c65f5fca4ae10cb35b382403d7
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-05 21:30:16 +0530

    Delete kafka-avro-pipeline/consumer directory

--- kafka-avro-pipeline/consumer/transformer.py
+++
@@ -1,7 +0,0 @@
-def transform(record):
-    record["category"] = record["category"].lower()
-
-    if record["category"] == "category a":
-        record["price"] = round(record["price"] * 0.5, 2)
-
-    return record
--- kafka-avro-pipeline/consumer/writer.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def write_json(record, instance_id):
-    os.makedirs("data/output", exist_ok=True)
-
-    with open(f"data/output/consumer_{instance_id}.json", "a") as f:
-        f.write(json.dumps(record) + "\n")
