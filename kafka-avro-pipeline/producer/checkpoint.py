5bff409db2ba48f5830240ea43f2d8b7c66923f1
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-05 19:44:45 +0530

    Update checkpoint.py

--- kafka-avro-pipeline/producer/checkpoint.py
+++ kafka-avro-pipeline/producer/checkpoint.py
@@ -1,13 +1,14 @@
 import json
+import os
 
 FILE = "producer/state.json"
 
 def get_last_ts():
-    try:
-        with open(FILE) as f:
-            return json.load(f)["last_ts"]
-    except:
+    if not os.path.exists(FILE):
         return "1970-01-01 00:00:00"
+
+    with open(FILE) as f:
+        return json.load(f).get("last_ts")
 
 def update_last_ts(ts):
     with open(FILE, "w") as f:
