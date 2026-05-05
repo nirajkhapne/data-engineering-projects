930d5ae799b3833a345c8b5c0da660d7dadc3385
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-05 20:11:55 +0530

    Update consumer.py

--- kafka-avro-pipeline/consumer/consumer.py
+++ kafka-avro-pipeline/consumer/consumer.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from confluent_kafka import DeserializingConsumer
 from confluent_kafka.serialization import StringDeserializer
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -6,9 +8,7 @@ from confluent_kafka.schema_registry.avro import AvroDeserializer
 from consumer.transformer import transform
 from consumer.writer import write_json
 from consumer.dlq_producer import send_to_dlq
-
-import os
-import sys
+from consumer.dedup_store import load_ids, is_processed, mark_processed
 
 def run(instance_id):
 
@@ -18,17 +18,18 @@ def run(instance_id):
 
     schema_str = open("schemas/product.avsc").read()
 
-    avro_deserializer = AvroDeserializer(schema_registry, schema_str)
-
     consumer = DeserializingConsumer({
         "bootstrap.servers": os.getenv("KAFKA_BOOTSTRAP"),
         "group.id": "product_group",
+        "enable.auto.commit": False,
         "auto.offset.reset": "earliest",
         "key.deserializer": StringDeserializer(),
-        "value.deserializer": avro_deserializer
+        "value.deserializer": AvroDeserializer(schema_registry, schema_str)
     })
 
     consumer.subscribe(["product_updates"])
+
+    processed_ids = load_ids()
 
     while True:
         msg = consumer.poll(1.0)
@@ -36,13 +37,24 @@ def run(instance_id):
         if msg is None or msg.error():
             continue
 
+        record = msg.value()
+        record_id = record["ID"]
+
+        # 🔥 DEDUP CHECK
+        if is_processed(record_id, processed_ids):
+            continue
+
         try:
-            record = transform(msg.value())
+            record = transform(record)
 
             write_json(record, instance_id)
 
+            mark_processed(record_id, processed_ids)
+
+            consumer.commit(message=msg)
+
         except Exception as e:
-            send_to_dlq(msg.value(), str(e))
+            send_to_dlq(record, str(e))
 
 
 if __name__ == "__main__":
