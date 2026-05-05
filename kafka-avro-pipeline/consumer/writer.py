264898c07b505244a3ef32f00c4cfee6b622f1e7
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-05 20:14:06 +0530

    Update writer.py

--- kafka-avro-pipeline/consumer/writer.py
+++ kafka-avro-pipeline/consumer/writer.py
@@ -2,9 +2,7 @@ import json
 import os
 
 def write_json(record, instance_id):
-    path = f"data/output/consumer_{instance_id}.json"
-
     os.makedirs("data/output", exist_ok=True)
 
-    with open(path, "a") as f:
+    with open(f"data/output/consumer_{instance_id}.json", "a") as f:
         f.write(json.dumps(record) + "\n")
