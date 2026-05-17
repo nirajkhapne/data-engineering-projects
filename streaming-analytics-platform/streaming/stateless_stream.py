72beb01e6d35655251c79167507bcffe94c9ee36
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-17 19:30:35 +0530

    Update stateless_stream.py

--- streaming-analytics-platform/streaming/stateless_stream.py
+++ streaming-analytics-platform/streaming/stateless_stream.py
@@ -2,19 +2,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 
 from schemas.user_schema import schema
-from configs.settings import settings
 
 spark = SparkSession.builder \
-    .appName("StatelessStreaming") \
+    .appName("StatelessStream") \
     .getOrCreate()
 
-kafka_df = spark.readStream \
+df = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", settings.KAFKA_BOOTSTRAP) \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "user_topic") \
     .load()
 
-parsed = kafka_df.selectExpr("CAST(value AS STRING)") \
+parsed = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
