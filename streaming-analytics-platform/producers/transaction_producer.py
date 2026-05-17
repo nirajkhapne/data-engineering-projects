cc27f93acc716f8a17f95dc1f98775bc9da765db
Niraj Khapne <nirajrkhapne@gmail.com>
2026-05-17 19:28:24 +0530

    Create transaction_producer.py

---
+++ streaming-analytics-platform/producers/transaction_producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json
+import time
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+with open("data/user_transactions.json") as f:
+
+    data = json.load(f)
+
+    for record in data:
+
+        producer.send("transactions_topic", value=record)
+
+        print("Published:", record)
+
+        time.sleep(2)
+
+producer.flush()
