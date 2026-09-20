b46bcd7f7f352fcc06ec11dd8f466b89f119642a
Niraj Khapne <nirajrkhapne@gmail.com>
2026-09-21 01:03:00 +0530

    Delete kafka-avro-pipeline directory

--- kafka-avro-pipeline/monitoring/metrics.py
+++
@@ -1,6 +0,0 @@
-from prometheus_client import start_http_server, Counter
-
-messages_processed = Counter('messages_processed', 'Total processed')
-
-def start():
-    start_http_server(8000)
--- kafka-avro-pipeline/spark/transform.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql.functions import col, lower, when
-
-def transform_df(df):
-    return df.withColumn("category", lower(col("category"))) \
-        .withColumn(
-            "price",
-            when(col("category") == "category a", col("price") * 0.5)
-            .otherwise(col("price"))
-        )
