f6f459ba8ab474d5b18b810c30b2f6b2903ee89b
Niraj Khapne <nirajrkhapne@gmail.com>
2026-09-20 11:00:03 +0530

    Update order_schema.py

--- streaming-analytics-platform/schemas/order_schema.py
+++ streaming-analytics-platform/schemas/order_schema.py
@@ -1,17 +1,17 @@
-from pyspark.sql.types import *
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 
 order_schema = StructType([
-    StructField("order_id", StringType(), True),
+    StructField("order_id", StringType(), False),
     StructField("order_date", StringType(), True),
     StructField("created_at", StringType(), True),
     StructField("customer_id", StringType(), True),
-    StructField("amount", IntegerType(), True)
+    StructField("amount", IntegerType(), True),
 ])
 
 payment_schema = StructType([
-    StructField("payment_id", StringType(), True),
-    StructField("order_id", StringType(), True),
+    StructField("payment_id", StringType(), False),
+    StructField("order_id", StringType(), False),
     StructField("payment_date", StringType(), True),
     StructField("created_at", StringType(), True),
-    StructField("amount", IntegerType(), True)
+    StructField("amount", IntegerType(), True),
 ])
